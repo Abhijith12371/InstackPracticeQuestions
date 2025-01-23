321ca2f18212ec4a2bc57e8a7097dca102d27a9c
Abhijith12371 <abpokeunite@gmail.com>
2025-01-23 22:34:51 +0530

    firstCommit

---
+++ print_the_previous_number.py
@@ -0,0 +1,20 @@
+def sumNum(num):
+    # Type your code here
+    for i in range(num):
+      print(f"{i}+{i-1}={i+(i-1)}")
+
+
+# Drive Code - complete it
+number_max_range=int(input())
+sumNum(number_max_range)    def sumNum(num):
+    # Type your code here
+    for i in range(0,num):
+      if i==0:
+        print("0 0 0")
+      if i>0:
+        print(f"{i} {i-1} {i+(i-1)}")
+
+
+# Drive Code - complete it
+number_max_range=int(input())
+sumNum(number_max_range)    
